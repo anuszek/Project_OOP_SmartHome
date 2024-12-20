0ac84edae409c6e0153cfcbdc774a5731bce3197
kyn2115 <rybakmaciej@student.agh.edu.pl>
2024-12-20 19:36:15 +0100

    smth dont hab ideas rn :(

---
+++ src/SmartHomeSystem/SmartDevice.java
@@ -0,0 +1,11 @@
+package SmartHomeSystem;
+
+public class SmartDevice {
+    private String deviceName;
+    private String deviceIPAddress;
+    private boolean state; // true == on; false == off
+
+    public void sendCommand(String command) {
+    }
+
+}
