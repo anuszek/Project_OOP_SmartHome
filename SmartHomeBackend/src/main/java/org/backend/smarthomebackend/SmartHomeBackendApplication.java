ea073ad9bc730a7489ed263b80334e346bb841a9
unknown <mjanuszek@student.agh.edu.pl>
2024-12-29 12:27:04 +0100

    java spring boot

---
+++ SmartHomeBackend/src/main/java/org/backend/smarthomebackend/SmartHomeBackendApplication.java
@@ -0,0 +1,13 @@
+package org.backend.smarthomebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartHomeBackendApplication.class, args);
+    }
+
+}
