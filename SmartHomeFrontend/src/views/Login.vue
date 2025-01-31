be6f4141e73fb94550e2c759b82810381d6a4fa0
unknown <mjanuszek@student.agh.edu.pl>
2025-01-31 21:51:37 +0100

    devices client side, pls add DB updates

--- SmartHomeFrontend/src/views/Login.vue
+++ SmartHomeFrontend/src/views/Login.vue
@@ -37,7 +37,7 @@ export default {
         password: this.password
       });
       
-      await axios.get('http://localhost:8080/api/login_data')
+      await axios.get('http://localhost:8080/api/login')
         .then((response) => {
           this.data = response.data;
         })
